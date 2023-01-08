0316c689a1bd459c8bd8e931a8f407aac54cbd6d
villa <villalon2511@gmail.com>
2023-01-08 16:20:32 +0100

    Añadido módulo utilidades.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import utilidades as ut
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Hola Mundo')
+    ut.saludo()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
